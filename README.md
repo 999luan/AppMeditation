2fe770f1f756ae56cc9f15c40772899e4220f535
Luan Carlos <tatalc4e@gmail.com>
2023-01-13 21:36:27 -0300

    readme feito

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# MeditationApp
-# AppMeditation
+Projeto Readme
+Este projeto é um simples player de música que permite ao usuário reproduzir/pausar, além de selecionar uma duração para a música e exibir o tempo restante. Ele é específico para uma aplicação de meditação sonora.
+
+A classe App controla toda a funcionalidade do player de música. Ela usa JavaScript para selecionar e manipular elementos do DOM e adicionar listeners de eventos para interações do usuário. A classe possui vários métodos que lidam com diferentes ações, como initEventListeners que configura todos os listeners de eventos para os botões e música, selectSound que muda a música e o vídeo quando um botão é clicado, checkPlay que alterna o estado de reprodução/pausa da música, selectDuration que muda a duração da música e animate que atualiza o progresso visual da música e o tempo restante.
+
+A classe também usa os atributos data-sound e data-video no HTML dos botões para obter a fonte da música e do vídeo.
+
+O projeto também usa ícones SVG para o botão de reprodução/pausa.
+
+A classe também tem um construtor que configura o estado inicial do player, como definir o strokeDashoffset e strokeDasharray do elemento de contorno para corresponder à duração da música.
+
+A classe também tem a variável duracaoFalsa que é usada para definir a duração da música, e a variável outlineLength que é usada para controlar a animação da barra de progresso.
+
+Uso
+Para usar este projeto, basta abrir o arquivo HTML em um navegador web e usar os botões para reproduzir/pausar a música e selecionar uma duração para a música. O tempo restante da música será exibido e a barra de progresso mostrará a posição atual da música.
+
+Informações adicionais
+Este projeto foi construído com as seguintes tecnologias:
+
+HTML
+CSS
+JavaScript
+SVG
+Tenha em mente que este projeto é um simples conceito de prova e pode não ser adequado para uso em produção.
--- temp.js
+++ temp.js
@@ -84,3 +84,115 @@ const app = () => {
 };
 
 app();
+
+//modelo em classes #1
+
+class App {
+  constructor() {
+    this.song = document.querySelector(".song");
+    this.play = document.querySelector(".play");
+    this.outline = document.querySelector(".moving-outline circle");
+    this.video = document.querySelector(".vidContainer video");
+    this.sounds = document.querySelectorAll(".selecionaMusica button");
+    this.mostraTempo = document.querySelector(".mostraTempo");
+    this.selecionaTempo = document.querySelectorAll(".selecionaTempo button");
+    this.outlineLength = this.outline.getTotalLength();
+    this.duracaoFalsa = 600;
+
+    this.outline.style.strokeDasharray = this.outlineLength;
+    this.outline.style.strokeDashoffset = this.outlineLength;
+
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    this.sounds.forEach((sound) => {
+      sound.addEventListener("click", (e) => this.selectSound(e));
+    });
+
+    this.play.addEventListener("click", () => {
+      this.checkPlay(this.song);
+    });
+
+    this.selecionaTempo.forEach((option) => {
+      option.addEventListener("click", this.selectDuration.bind(this));
+    });
+
+    this.song.ontimeupdate = this.animate.bind(this);
+  }
+
+  selectSound(e) {
+    // Get the button that was clicked
+    const button = e.target;
+
+    // Check if the button already has the 'disabled' class
+    if (button.classList.contains("disabled")) return;
+
+    const sound = button.getAttribute("data-sound");
+    const video = button.getAttribute("data-video");
+
+    if (!sound || !video) {
+      console.error(
+        "Could not find sound or video source. Make sure to have the data-sound and data-video attributes correctly set on the buttons element"
+      );
+      return;
+
+      // Add the 'disabled' class to the button
+      button.classList.add("disabled");
+
+      this.song.src = sound;
+      this.video.src = video;
+
+      // Attach an event listener to the song's `canplay` event
+      this.song.addEventListener("canplay", () => {
+        // Remove the disabled class when the song is ready
+        button.classList.remove("disabled");
+      });
+    }
+  }
+  togglePlay() {
+    this.checkPlay(this.song);
+  }
+
+  selectDuration(e) {
+    this.duracaoFalsa = e.target.getAttribute("data-time");
+    this.mostraTempo.textContent = `${Math.floor(
+      this.duracaoFalsa / 60
+    )}:${Math.floor(this.duracaoFalsa % 60)}`;
+  }
+
+  checkPlay(song) {
+    if (song.paused) {
+      song.play();
+      this.video.play();
+      this.play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      this.video.pause();
+      this.play.src = "./svg/play.svg";
+    }
+  }
+
+  animate() {
+    let currentTime = this.song.currentTime;
+    let elapsed = this.duracaoFalsa - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    let progresso =
+      this.outlineLength -
+      (currentTime / this.duracaoFalsa) * this.outlineLength;
+    this.outline.style.strokeDashoffset = progresso;
+
+    this.mostraTempo.textContent = `${minutes}:${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+    if (currentTime >= this.duracaoFalsa) {
+      this.song.pause();
+      this.song.currentTime = 0;
+      this.play.src = "./svg/play.svg";
+      this.video.pause();
+    }
+  }
+}
+const myApp = new App();
